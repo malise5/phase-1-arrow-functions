91fd08e0418af3b33e1427c1bc44d88be04c1a3f
Halkano <kudezx@gmail.com>
2022-08-22 10:18:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000 / 100;
+}
+console.log(divide());
+
+const square = x => x*x;
+console.log(square(2));
+
+const add = (a,b)=> a+b;
+console.log(add(2,5));
